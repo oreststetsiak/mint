5bce7fc84d6d8450558aafc9ab5b64839540f59a
Orest Stetsiak <axeopa@gmail.com>
2015-10-26 13:56:01 +0200

    Default comment

--- linuxmint/sw.sh
+++ linuxmint/sw.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
-sudo apt-get install python-software-properties
 
+echo -e "INFO: Adding PPAs"
+
+sudo apt-get install python-software-properties > /dev/null 2>&1
 # PPAs
-sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
-sudo add-apt-repository -y ppa:vincent-c/cherrytree
-sudo add-apt-repository -y ppa:webupd8team/haguichi
-sudo add-apt-repository -y ppa:landronimirc/skippy-xd-daily
-sudo add-apt-repository -y ppa:numix/ppa
-sudo add-apt-repository -y ppa:noobslab/themes
-sudo add-apt-repository -y ppa:noobslab/icons
-sudo add-apt-repository -y ppa:numix/ppa
-sudo add-apt-repository -y ppa:snwh/pulp
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3 > /dev/null 2>&1
+sudo add-apt-repository -y ppa:vincent-c/cherrytree > /dev/null 2>&1
+sudo add-apt-repository -y ppa:webupd8team/haguichi > /dev/null 2>&1
+sudo add-apt-repository -y ppa:landronimirc/skippy-xd-daily > /dev/null 2>&1
+sudo add-apt-repository -y ppa:numix/ppa > /dev/null 2>&1
+sudo add-apt-repository -y ppa:noobslab/themes > /dev/null 2>&1
+sudo add-apt-repository -y ppa:noobslab/icons > /dev/null 2>&1
+sudo add-apt-repository -y ppa:numix/ppa > /dev/null 2>&1
+sudo add-apt-repository -y ppa:snwh/pulp > /dev/null 2>&1
 
 sleep 5
 
+echo -e "INFO: Making sudo"
+
 # sudo 
-usrnm=`whoami`
-sudo touch /etc/sudoers.d/$usrnm
-sudo chown $usrnm:$usrnm /etc/sudoers.d/$usrnm
-sudo echo "$usrnm ALL = NOPASSWD: ALL" > /etc/sudoers.d/$usrnm
-sudo chown root:root /etc/sudoers.d/$usrnm
-sudo chmod 0440 /etc/sudoers.d/$usrnm
-sudo more /etc/sudoers.d/$usrnm
+usrnm=`whoami` > /dev/null 2>&1
+sudo touch /etc/sudoers.d/$usrnm > /dev/null 2>&1
+sudo chown $usrnm:$usrnm /etc/sudoers.d/$usrnm > /dev/null 2>&1
+sudo echo "$usrnm ALL = NOPASSWD: ALL" > /etc/sudoers.d/$usrnm > /dev/null 2>&1
+sudo chown root:root /etc/sudoers.d/$usrnm > /dev/null 2>&1
+sudo chmod 0440 /etc/sudoers.d/$usrnm > /dev/null 2>&1
+sudo more /etc/sudoers.d/$usrnm > /dev/null 2>&1
 
+echo -e "INFO: Fixing supporting for locale en_us utf8"
 # fix linux mint no support for locale en_us utf8
-sudo locale-gen --purge --no-archive
+sudo locale-gen --purge --no-archive > /dev/null 2>&1
 
-
+echo -e "INFO: Installing software ..."
 # soft
 sudo apt-get update
 sudo apt-get install -y \
@@ -80,8 +85,11 @@ winetricks \
 nagstamon \
 meld \
 libreoffice \
-skype
+skype > /dev/null 2>&1
 
 
-sudo apt-get update && sudo apt-get -y upgrade
+echo -e "INFO: Updating && upgrading"
+sudo apt-get update > /dev/null 2>&1 && sudo apt-get -y upgrade > /dev/null 2>&1
+
+echo -e "INFO: Succeed!"
 
