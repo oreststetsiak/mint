7167ae8e5430cb8d1f0c04f515f3e1f6f74b49d7
Orest Stetsiak <axeopa@gmail.com>
2016-11-20 18:28:08 +0200

    sublime

--- linuxmint/sw.sh
+++ linuxmint/sw.sh
@@ -35,7 +35,6 @@ faience-icon-theme \
 firefox \
 git \
 gitg \
-goldendict \
 gnupg2 \
 htop \
 inkscape \
@@ -70,7 +69,7 @@ virt-viewer
 
 # bashrc
 rm -rf ~/.bashrc
-cp .bashrc ~/ 
+cp .bashrc ~/
 
 
 echo -e "INFO: Configuring terminal!"
@@ -90,14 +89,9 @@ fi
 cat sl_usr.conf > ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 cat sl_pkgs.conf > ~/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings
 
-# goldendict
-echo -e "INFO: Configuring goldendict!"
-sudo rm -rf ~/.goldendict
-cp -rf .goldendict ~/
-
 echo -e "INFO: Succeed!"
 echo -e "\n\n\n"
 echo -e "INFO: gnome-screensaver-command --lock - for lock screen\n"
 echo -e "INFO: For installing Pachage Control open subline, press 'ctrl + ~'' and paste code mensioned below: \n\n
-import urllib.request,os,hashlib; h = 'df21e130d211cfc94d9b0905775a7c0f' + '1e3d39e33b79698005270310898eea76'; pf = 'Package Control.sublime-package'; ipp = sublime.installed_packages_path(); urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) ); by = urllib.request.urlopen( 'http://packagecontrol.io/' + pf.replace(' ', '%20')).read(); dh = hashlib.sha256(by).hexdigest(); print('Error validating download (got %s instead of %s), please try manual install' % (dh, h)) if dh != h else open(os.path.join( ipp, pf), 'wb' ).write(by) 
+import urllib.request,os,hashlib; h = 'df21e130d211cfc94d9b0905775a7c0f' + '1e3d39e33b79698005270310898eea76'; pf = 'Package Control.sublime-package'; ipp = sublime.installed_packages_path(); urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) ); by = urllib.request.urlopen( 'http://packagecontrol.io/' + pf.replace(' ', '%20')).read(); dh = hashlib.sha256(by).hexdigest(); print('Error validating download (got %s instead of %s), please try manual install' % (dh, h)) if dh != h else open(os.path.join( ipp, pf), 'wb' ).write(by)
 \n"
