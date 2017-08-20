357bcb2097f887e4691b424a5dd09a74afb79e90
Orest Stetsiak <ext-orest.stetsiak@here.com>
2017-08-20 13:01:07 +0300

    virt removed, removed some output

--- mint/sw.sh
+++ mint/sw.sh
@@ -51,15 +51,7 @@ tree \
 wget \
 wine \
 winetricks \
-wireshark \
-gnome-screensaver \
-qemu-system \
-qemu-utils \
-libvirt-bin \
-virt-manager \
-python-spice-client-gtk \
-virt-viewer
-
+wireshark
 
 # bashrc
 rm -rf ~/.bashrc
@@ -85,7 +77,7 @@ cat sl_pkgs.conf > ~/.config/sublime-text-3/Packages/User/Package\ Control.subli
 
 echo -e "INFO: Succeed!"
 echo -e "\n\n\n"
-echo -e "INFO: gnome-screensaver-command --lock - for lock screen\n"
+
 echo -e "INFO: For installing Pachage Control open subline, press 'ctrl + ~'' and paste code mensioned below: \n\n
 import urllib.request,os,hashlib; h = 'df21e130d211cfc94d9b0905775a7c0f' + '1e3d39e33b79698005270310898eea76'; pf = 'Package Control.sublime-package'; ipp = sublime.installed_packages_path(); urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) ); by = urllib.request.urlopen( 'http://packagecontrol.io/' + pf.replace(' ', '%20')).read(); dh = hashlib.sha256(by).hexdigest(); print('Error validating download (got %s instead of %s), please try manual install' % (dh, h)) if dh != h else open(os.path.join( ipp, pf), 'wb' ).write(by)
 \n"
