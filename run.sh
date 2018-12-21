aea0f85ab67f0ae5d562c464a85403c0dc92c0a2
Orest Stetsiak <orest.stetsiak@gmail.com>
2018-12-21 21:38:50 +0200

    update

--- run.sh
+++ run.sh
@@ -37,18 +37,10 @@ shutter \
 tree \
 wireshark
 
-# bashrc
-rm -rf ~/.bashrc
-cp .bashrc ~/
 
 cp -rf .themes ~/.themes
 
-echo -e "INFO: Configuring terminal!"
-ls ~/.config/xfce4/terminal
-if [ $? -ne 0 ]; then
-    mkdir -p ~/.config/xfce4/terminal
-fi
-cat term > ~/.config/xfce4/terminal/terminalrc
+echo "PS1='\[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]$\[\033[0m\]'" >> ~/.bashrc
 
 cd ~/coding/
 git clone https://github.com/chriskempson/base16-xfce4-terminal
