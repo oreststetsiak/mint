9445fdb9fd99c132926051f579c1a994a7518052
Orest Stetsiak <orest.stetsiak@gmail.com>
2020-07-06 16:42:31 +0300

    linux mint 20 update

--- kube_stuff.sh
+++ kube_stuff.sh
@@ -9,9 +9,5 @@ unzip awscliv2.zip
 sudo ./aws/install
 
 curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/aws-iam-authenticator
-curl -o aws-iam-authenticator.sha256 https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/aws-iam-authenticator.sha256
-openssl sha1 -sha256 aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
-mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
-echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
-aws-iam-authenticator help
+sudo mv aws-iam-authenticator /usr/bin/
